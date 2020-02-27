27e3c9a587bb16ed07ec2fdb46bd6023790e12bd
Jack-Storment <jstorment1998@gmail.com>
2020-02-27 18:11:06 -0500

    Changes

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers_table (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar(255),
+  burger_devoured BOOLEAN
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_table (burger_name,burger_devoured) VALUES ("bacon cheese burger",false);
+INSERT INTO burgers_table (burger_name,burger_devoured) VALUES ("impossible burger",false);
+INSERT INTO burgers_table (burger_name,burger_devoured) VALUES ("turkey burger",false);
